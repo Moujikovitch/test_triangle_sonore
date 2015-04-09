b335bc4f057bcf37f5dcbfbf04e20116459ae5c2
moujikovitch <sbst.legall@gmail.com>
2015-04-09 15:43:02 +0200

    upload des données triangle mapping

---
+++ bricolage.js
@@ -0,0 +1,16 @@
+
+function $(x) {return document.getElementById(x);}
+function play_music() {
+  $('music').innerHTML = '<object type="audio/mpeg" width="0" height="0" data="/static/download/roulement_tambour.mp3"><param name="filename" value="roulement_tambour.mp3" /><param name="autostart" value="true" /><param name="loop" value="false" /></object>';
+}
+
+function stop_music() {
+  $('music').innerHTML = '';
+}
+
+
+
+modifier INVITATION => AFTERWORK
+SimplonMars => SIMPLonMARS
+mettre prefecture en "projet soutenu par" et plus petit
+mettre école centrale de marseille avant l'adresse
